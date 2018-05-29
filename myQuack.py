6236a17fce2a85092479d79d426fe049435f33ff
ManuConcepBrito <33829944+ManuConcepBrito@users.noreply.github.com>
2018-05-29 17:39:58 +1000

    Correct errors

--- myQuack.py
+++ myQuack.py
@@ -1,10 +1,10 @@
 '''
 
-Scaffolding code for the Machine Learning assignment. 
+Scaffolding code for the Machine Learning assignment.
 
 You should complete the provided functions and add more functions and classes as necessary.
- 
-Write a main function that calls the different functions to perform the required tasks 
+
+Write a main function that calls the different functions to perform the required tasks
 and repeat your experiments.
 
 
@@ -13,13 +13,16 @@ and repeat your experiments.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn.svm import SVC
 from sklearn import preprocessing, decomposition
+
 np.random.seed(10)
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -28,21 +31,22 @@ def my_team():
     '''
     Return the list of the team members of this assignment submission as a list
     of triplet of the form (student_number, first_name, last_name)
-    
+
     '''
-    return [ (10144820, 'Petr', 'Ungar'), (1234568, 'Grace', 'Hopper') ]
+    return [(10144820, 'Petr', 'Ungar'), (1234568, 'Grace', 'Hopper')]
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 def prepare_dataset(dataset_path):
-    '''  
-    Read a comma separated text file where 
-	- the first field is a ID number 
+    '''
+    Read a comma separated text file where
+	- the first field is a ID number
 	- the second field is a class label 'B' or 'M'
 	- the remaining fields are real-valued
 
-    Return two numpy arrays X and y where 
+    Return two numpy arrays X and y where
 	- X is two dimensional. X[i,:] is the ith example
 	- y is one dimensional. y[i] is the class label of X[i,:]
           y[i] should be set to 1 for 'M', and 0 for 'B'
@@ -52,11 +56,11 @@ def prepare_dataset(dataset_path):
     @return
 	X,y
     '''
-    dataframe = pd.read_csv(dataset_path, header = None)
+    dataframe = pd.read_csv(dataset_path, header=None)
     # Get the class label ('M' or 'B' and convert it into a numpy array
     y = np.array(dataframe.loc[:, 1])
     # Get the X data. We skip the first to columns (i.e., ID and class label)
-    X = np.array(dataframe.loc[:,2:])
+    X = np.array(dataframe.loc[:, 2:])
     X.astype('float32')
     # Search where there are M and where B
     idx_M = np.argwhere(y == 'M')
@@ -64,21 +68,22 @@ def prepare_dataset(dataset_path):
     # Convert 'M' to 1 and 'B' to 0
     y[idx_M] = 1
     y[idx_B] = 0
-    y = y.astype('int') #Ask him about this line
+    y = y.astype('int')  # Ask him about this line
     # Check shapes before returning
     if y.shape[0] != X.shape[0]:
-        raise ValueError("Unequal number of columns between target (%d columns) and data vector (%d columns)." % (y.shape[0], X.shape[0]))
+        raise ValueError("Unequal number of columns between target (%d columns) and data vector (%d columns)." % (
+        y.shape[0], X.shape[0]))
 
     return X, y
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_NB_classifier(X_training, y_training):
-
-    '''  
+    '''
     Build a Naive Bayes classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
@@ -91,14 +96,13 @@ def build_NB_classifier(X_training, y_training):
     return clf
 
 
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_DT_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Decision Tree classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
@@ -112,36 +116,38 @@ def build_DT_classifier(X_training, y_training):
     clf.fit(X_training, y_training)
     return clf
 
+
 def tune_DT_classifier(builder, X_train, y_train):
-    '''  
+    '''
     Find best params for a Decision Tree classifier based on the training set X_training, y_training.
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
     @return
 	best params for decision tree (dictionary)
     '''
-    params = {"criterion":["gini","entropy"],
-              "splitter":["best","random"],
-              "min_samples_split": [a for a in range(20,1,-1)]}
-    
-    tree_cross_validation = RandomizedSearchCV(builder,params,cv=5)
-    
+    params = {"criterion": ["gini", "entropy"],
+              "splitter": ["best", "random"],
+              "min_samples_split": [a for a in range(20, 1, -1)]}
+
+    tree_cross_validation = RandomizedSearchCV(builder, params, cv=5)
+
     tree_cross_validation.fit(X_train, y_train)
-        
+
     print("\n=========================Best params for DT:============================\n|",
           tree_cross_validation.best_params_,
           "|\n========================================================================")
-    
+
     return tree_cross_validation.best_params_
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def build_NN_classifier(X_training, y_training, neighbors = 1):
-    '''  
+def build_NN_classifier(X_training, y_training, neighbors=1):
+    '''
     Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
@@ -151,22 +157,26 @@ def build_NN_classifier(X_training, y_training, neighbors = 1):
     clf = KNeighborsClassifier(n_neighbors=neighbors)
     clf = clf.fit(X_training, y_training)
     return clf
+
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def build_SVM_classifier(X_training, y_training, C_value = 1 ):
-    '''  
+def build_SVM_classifier(X_training, y_training, C_value=1):
+    '''
     Build a Support Vector Machine classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
 	clf : the classifier built in this function
     '''
-    clf = SVC(C=C_value, random_state= 10) # Set random seed for reproducibility
+    clf = SVC(C=C_value, random_state=10)  # Set random seed for reproducibility
     clf = clf.fit(X_training, y_training)
     return clf
+
+
 def cross_validate_model(builder, X_train, y_train, hyperparameter_list):
     """
     @ params
@@ -178,7 +188,7 @@ def cross_validate_model(builder, X_train, y_train, hyperparameter_list):
     @return
     Model
     """
-    final_score =[]
+    final_score = []
     models = []
     for hyperparameter in hyperparameter_list:
         clf = builder(X_train, y_train, hyperparameter)
@@ -189,7 +199,8 @@ def cross_validate_model(builder, X_train, y_train, hyperparameter_list):
 
     best_model = models[final_score.index(max(final_score))]
     best_hyperparameter = hyperparameter_list[final_score.index(max(final_score))]
-    return best_model,best_hyperparameter, final_score
+    return best_model, best_hyperparameter, final_score
+
 
 def test_model(model, X_test, y_test):
     """
@@ -234,23 +245,20 @@ def visualize_PCA(Data, y_training):
 
     """
     fig, ax = plt.subplots(figsize=(10, 10), dpi=100)
-    ax.scatter(Data[:,0], Data[:,1])
+    ax.scatter(Data[:, 0], Data[:, 1])
     for i, label in enumerate(y_training):
-        plt.annotate(label,(Data[i, 0], Data[i, 1]))
+        plt.annotate(label, (Data[i, 0], Data[i, 1]))
         plt.xlabel('Principal Component 1')
         plt.ylabel('Principal Component 2')
     plt.show()
-
-
-
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if __name__ == "__main__":
     path = "C:/Users/BWH\Desktop/Clase/Australia/Artificial Intelligence/Machine learning/medical_records.data"
-    X, y = prepare_dataset(dataset_path= path)
-    #SHUFFLE THE DATA
+    X, y = prepare_dataset(dataset_path=path)
+    # SHUFFLE THE DATA
     X_training, X_test, y_training, y_test = train_test_split(X, y, test_size=0.25)
     # Compute the mean and std for the training data. Apply to both training and test.
     scaler = preprocessing.StandardScaler().fit(X_training)
@@ -261,18 +269,19 @@ if __name__ == "__main__":
     # Evaluate hyperparameters
     print('Evaluating hyperparameters...\n')
     print('...Number of neighbors in KNN...\n')
-    neighbors = list(np.arange(300) + 1)
-    clf_nn, best_neighbor, acc_per_neighbor = cross_validate_model(build_NN_classifier, X_training, y_training, neighbors)
+    neighbors = [1, 2, 3, 10, 50, 100, 150, 200, 250, 300]
+    clf_nn, best_neighbor, acc_per_neighbor = cross_validate_model(build_NN_classifier, X_training, y_training,
+                                                                   neighbors)
     plt.bar(neighbors, acc_per_neighbor)
-    plt.title('Accuracy per Neighbor')
-    plt.xlabel('Neighbors')
-    plt.ylabel('Accuracy')
+    plt.title('Accuracy per Neighbor', fontsize=20)
+    plt.xlabel('Neighbors', fontsize=20)
+    plt.ylabel('Accuracy', fontsize=20)
     print('Neighbours\t Accuracy\n', '\n'.join([' %d\t\t\t%f %%' % item for item in zip(neighbors, acc_per_neighbor)]))
-    print('\n Best Neighbor is: %d' %(best_neighbor))
+    print('\n Best Neighbor is: %d' % best_neighbor)
     # C penalty value
     print('\n...C penalty value in SVM...\n')
     C_list = [0.001, 0.01, 0.1, 1, 10, 100]
-    clf_svm, best_C_value, acc_per_C = cross_validate_model(build_SVM_classifier, X_training,y_training, C_list)
+    clf_svm, best_C_value, acc_per_C = cross_validate_model(build_SVM_classifier, X_training, y_training, C_list)
     print('C value\t Accuracy\n', '\n'.join([' %1.3f\t\t%f %%' % item for item in zip(C_list, acc_per_C)]))
     # Hyperparameter for decision tree
     clf_dt = build_DT_classifier(X_training, y_training)
@@ -287,6 +296,6 @@ if __name__ == "__main__":
     print('Naïve Bayes model achieved: %1.3f %% accuracy in the test set' % acc_nb)
     print('KNN model achieved: %1.3f %% accuracy in the test set' % acc_knn)
     print('SVM model achieved: %1.3f %% accuracy in the test set' % acc_svm)
-    print('Decission tree achieved: %1.3f % accuracy in the test set' % acc_dt)
+    print('Decission tree achieved: %1.3f %% accuracy in the test set' % acc_dt)
 
     plt.show()
